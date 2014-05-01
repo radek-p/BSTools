7f464b2687041cf42ef7f59a66c4e47ccb3d2ecb
przemek <przemek@4ce36bd9-3a78-4973-a166-986cbda2e52d>
2014-05-01 16:23:41 +0000

    Changes in submesh extracting procedures
    git-svn-id: https://femdk.l5.pk.edu.pl/bstools/BSTools/trunk@63 4ce36bd9-3a78-4973-a166-986cbda2e52d

--- src/bsmesh/bsmesh31d.c
+++ src/bsmesh/bsmesh31d.c
@@ -3,7 +3,7 @@
 /* This file is a part of the BSTools package                                */
 /* written by Przemyslaw Kiciak                                              */
 /* ///////////////////////////////////////////////////////////////////////// */
-/* (C) Copyright by Przemyslaw Kiciak, 2013                                  */
+/* (C) Copyright by Przemyslaw Kiciak, 2013, 2014                            */
 /* this package is distributed under the terms of the                        */
 /* Lesser GNU Public License, see the file COPYING.LIB                       */
 /* ///////////////////////////////////////////////////////////////////////// */
@@ -18,29 +18,9 @@
 #include "bsmesh.h"
 #include "bsmprivate.h"
 
-/*#define LZZ*/
+/*#define DEBUG*/
 
-#ifdef LZZ
-static int liczbaZmianZnaku ( int *tablica, int dlugoscTablicy )
-{
-  int pom, i;
-
-  if ( tablica[0] == tablica[dlugoscTablicy-1] )
-    pom = -1;
-  else
-    pom=0;
-  for ( i = 0; i < dlugoscTablicy-1; i++ ) {
-    if ( tablica[i] != tablica[i+1] )
-      pom++;
-  }
-  if ( pom == -1 )
-    pom = 0;
-  return pom;
-} /*liczbaZmianZnaku*/
-#endif
-
-boolean bsm_ExtractSubmeshVNum ( int inv,
-                                 const BSMvertex *imv, const int *imvhei,
+boolean bsm_ExtractSubmeshVNum ( int inv, const BSMvertex *imv, const int *imvhei,
                                  int inhe, const BSMhalfedge *imhe,
                                  int infac, const BSMfacet *imfac, const int *imfhei,
                                  boolean *vtag,
@@ -52,10 +32,10 @@ boolean bsm_ExtractSubmeshVNum ( int inv,
   int *isHalfedgeInResultingNet;
   int *isFacetInResultingNet;
   int *isVertexInResultingNet;
-  int *tablicaPlusowIMinusow;
   int wielkoscomv, wielkoscomhe, wielkoscomfac;
 
   sp = pkv_GetScratchMemTop ();
+  isHalfedgeInResultingNet = pkv_GetScratchMemi ( inhe );
   isHalfedgeInResultingNet = pkv_GetScratchMemi ( inhe );
   isFacetInResultingNet = pkv_GetScratchMemi ( infac );
   isVertexInResultingNet = pkv_GetScratchMemi ( inv );
@@ -88,20 +68,21 @@ boolean bsm_ExtractSubmeshVNum ( int inv,
 skip:
     continue;
   }
+
   for ( i = j = 0; i < inv; i++ ) {
     if ( isVertexInResultingNet[i] == 0 )
-      isVertexInResultingNet[i] = j++;
+    isVertexInResultingNet[i] = j++;
   }
-#ifndef LZZ
+
   for ( i = 0; i < inv; i++ ) {
     vertexCopiesCounter[i] = 0;
     if ( isVertexInResultingNet[i] >= 0 ) {
       f = imv[i].firsthalfedge;
       d = imv[i].degree;
-/* tu jest blad; dla wierzcholka wewnetrznego, ktory pozostaje wewnetrzny, */
-/* vertexCopiesCounter dostaje wartosc 0 zamiast 1. Trzeba najpierw sprawdzic, */
-/* czy wierzcholek jest wewnetrzny, a nastepnie osobno policzyc zmiany znaku */
-/* dla wewnetrznego i brzegowego */
+            /* tu jest blad; dla wierzcholka wewnetrznego, ktory pozostaje wewnetrzny, */
+            /* vertexCopiesCounter dostaje wartosc 0 zamiast 1. Trzeba najpierw sprawdzic, */
+            /* czy wierzcholek jest wewnetrzny, a nastepnie osobno policzyc zmiany znaku */
+            /* dla wewnetrznego i brzegowego */
       for ( j = 0; j < d; j++ ) {
         if ( isHalfedgeInResultingNet[imvhei[f+j]] >= 0 ) {
           if ( imhe[imvhei[f+j]].otherhalf==-1 ||
@@ -109,29 +90,11 @@ skip:
             vertexCopiesCounter[i] ++;
         }
       }
-      if ( vertexCopiesCounter[i] > 1 )
-        goto failure;
-      wielkoscomv ++;
+      if ( vertexCopiesCounter[i] == 0 )
+        vertexCopiesCounter[i]=1;
+      wielkoscomv+=vertexCopiesCounter[i];
     }
   }
-#else
-        /* tutaj kod z liczeniem plusow i minusow */
-  for ( i = 0; i < inv; i++ ) {
-            /* wybieram podciag polkrawedzi takich ktore wychodza z danego wierzcholka */
-    f = imv[i].firsthalfedge;
-    d = imv[i].degree;
-    tablicaPlusowIMinusow = pkv_GetScratchMemi( d );
-    for ( j = 0; j < d; j++ ) {
-      if ( isHalfedgeInResultingNet[imvhei[f+j]] >= 0 )
-        tablicaPlusowIMinusow[j] = 1;
-      else
-        tablicaPlusowIMinusow[j] = -1;
-    }
-    if ( liczbaZmianZnaku ( tablicaPlusowIMinusow, d ) <= 1 )
-      goto failure;
-    wielkoscomv ++;
-  }
-#endif
 
   *onv = wielkoscomv;
   *onhe = wielkoscomhe;
@@ -144,25 +107,32 @@ failure:
   return false;
 } /*bsm_ExtractSubmeshVNum*/
 
-boolean bsm_ExtractSubmeshVd ( int spdimen, int inv,
-                               const BSMvertex *imv, const int *imvhei,
+boolean bsm_ExtractSubmeshVd ( int spdimen,
+                               int inv, const BSMvertex *imv, const int *imvhei,
                                double *iptc, int inhe, const BSMhalfedge *imhe,
                                int infac, const BSMfacet *imfac, const int *imfhei,
                                boolean *vtag,
-                               int *onv,  BSMvertex *omv,  int *omvhei,
-                               double *optc, int *onhe,  BSMhalfedge *omhe,
-                               int *onfac,  BSMfacet *omfac,  int *omfhei )
+                               int *onv, BSMvertex *omv, int *omvhei,
+                               double *optc, int *onhe, BSMhalfedge *omhe,
+                               int *onfac, BSMfacet *omfac, int *omfhei )
 {
-  void *sp;
-  int  d, f, i, j, k, l, m, ifhe, ofhe;
-  int *vertexCopiesCounter;
-  int *isHalfedgeInResultingNet;
-  int *isFacetInResultingNet;
-  int *isVertexInResultingNet;
-  int *tablicaPlusowIMinusow;
-  int wielkoscomv, wielkoscomhe, wielkoscomfac;
+  void        *sp;
+  int         d, f, i, j, k, l, omvLastFreeSlot, ifhe, ofhe, face,
+              currentVertex, heIter, omvheiIter, boundaryHalfedge, omvheiEntry;
+  int         *vertexCopiesCounter;
+  int         *isHalfedgeInResultingNet;
+  int         *isFacetInResultingNet;
+  int         *isVertexInResultingNet;
+  int         *V0, *V1;
+  boolean     isSecondOrNext;
+  BSMhalfedge heIn;
+  BSMhalfedge heOut;
+  int         zDrugiejStrony, a, pom;
 
+    /*przypisanie pamieci na tablice*/
   sp = pkv_GetScratchMemTop ();
+  V1 = pkv_GetScratchMemi ( inhe );
+  V0 = pkv_GetScratchMemi ( inhe );
   isHalfedgeInResultingNet = pkv_GetScratchMemi ( inhe );
   isFacetInResultingNet = pkv_GetScratchMemi ( infac );
   isVertexInResultingNet = pkv_GetScratchMemi ( inv );
@@ -170,6 +140,17 @@ boolean bsm_ExtractSubmeshVd ( int spdimen, int inv,
   if ( !isHalfedgeInResultingNet || !isFacetInResultingNet ||
        !isVertexInResultingNet || !vertexCopiesCounter )
     goto failure;
+
+  if ( !V0 || !V1 )
+    goto failure;
+
+    /*zainicjowanie tablic i zmiennych*/
+  for ( i = 0; i < inhe; i++ ) {
+    V0[i] = -10;
+    V1[i] = -10;
+  }
+  for ( i = 0; i < *onv; i++ )
+    omv[i].degree=0;
   for ( i = 0; i < inhe; i++ )
     isHalfedgeInResultingNet[i] = -1;
   for ( i = 0; i < infac; i++ )
@@ -177,7 +158,9 @@ boolean bsm_ExtractSubmeshVd ( int spdimen, int inv,
   for ( i = 0; i < inv; i++ )
     isVertexInResultingNet[i] = -1;
 
-  wielkoscomv = wielkoscomfac = wielkoscomhe = 0;
+  *onv = *onfac = *onhe = 0;
+
+    /*oznaczenie scian wierzcholkow i polkrawedzi wchodzacych do siatki wynikowej*/
   for ( i = k = l = 0; i < infac; i++ ) {
     f = imfac[i].firsthalfedge;
     d = imfac[i].degree;
@@ -190,25 +173,24 @@ boolean bsm_ExtractSubmeshVd ( int spdimen, int inv,
       isHalfedgeInResultingNet[imfhei[f+j]] = l++;
       isVertexInResultingNet[imhe[imfhei[f+j]].v0] = 0;
     }
-    wielkoscomfac ++;
-    wielkoscomhe += d;
+    (*onfac) ++;
+    (*onhe) += d;
 skip:
     continue;
   }
+
+    /*przenumerowanie wierzcholkow wchodzacych do siatki wynikowej*/
   for ( i = j = 0; i < inv; i++ ) {
     if ( isVertexInResultingNet[i] == 0 )
       isVertexInResultingNet[i] = j++;
   }
-#ifndef LZZ
+    /*liczenie kopii wierzcholkow*/
   for ( i = 0; i < inv; i++ ) {
-    vertexCopiesCounter[i] = 0;
+    vertexCopiesCounter[i] = -1;
     if ( isVertexInResultingNet[i] >= 0 ) {
+      vertexCopiesCounter[i]++;
       f = imv[i].firsthalfedge;
       d = imv[i].degree;
-/* tu jest blad; dla wierzcholka wewnetrznego, ktory pozostaje wewnetrzny, */
-/* vertexCopiesCounter dostaje wartosc 0 zamiast 1. Trzeba najpierw sprawdzic, */
-/* czy wierzcholek jest wewnetrzny, a nastepnie osobno policzyc zmiany znaku */
-/* dla wewnetrznego i brzegowego */
       for ( j = 0; j < d; j++ ) {
         if ( isHalfedgeInResultingNet[imvhei[f+j]] >= 0 ) {
           if ( imhe[imvhei[f+j]].otherhalf==-1 ||
@@ -216,67 +198,148 @@ skip:
             vertexCopiesCounter[i] ++;
         }
       }
-      if ( vertexCopiesCounter[i] > 1 )
-        goto failure;
-      wielkoscomv ++;
+      if ( vertexCopiesCounter[i] == 0 )
+        (*onv) ++;
+      (*onv) += vertexCopiesCounter[i];
     }
   }
-#else
-        /* tutaj kod z liczeniem plusow i minusow */
-  for ( i = 0; i < inv; i++ ) {
-            /* wybieram podciag polkrawedzi takich ktore wychodza z danego wierzcholka */
-    f = imv[i].firsthalfedge;
-    d = imv[i].degree;
-    tablicaPlusowIMinusow = pkv_GetScratchMemi( d );
-    for ( j = 0; j < d; j++ ) {
-      if ( isHalfedgeInResultingNet[imvhei[f+j]] >= 0 )
-        tablicaPlusowIMinusow[j] = 1;
-      else
-        tablicaPlusowIMinusow[j] = -1;
-    }
-    if ( liczbaZmianZnaku ( tablicaPlusowIMinusow, d ) <= 1 )
-      goto failure;
-    wielkoscomv ++;
-  }
+
+#ifdef DEBUG
+printf ( "VertexCopiesCounter\n" );
+for ( i = 0; i < inv; i++ ) {
+  printf ( "%d", vertexCopiesCounter[i] );
+  printf ( " " );
+}
 #endif
 
-/* wpisuje wierzcholki, w odpowiednim momencie je podwajajac */
-
-  /* przepisanie elementow nowej siatki */
-    /* omv, optc, omvhei */
-  for ( i = j = 0; i < inv; i++ ) {
-    j = isVertexInResultingNet[i];
-    if ( j >= 0 ) {
-      memcpy ( optc+j*spdimen, iptc+i*spdimen, spdimen*sizeof(double) );
-      ifhe = imv[i].firsthalfedge;
-      omv[j].degree = 0;
-      for ( k = 0; k < imv[i].degree; k++ ) {
-        if ( isHalfedgeInResultingNet[imvhei[ifhe+k]] >= 0 )
-          omv[j].degree ++;
-      }
-      omv[j].firsthalfedge = ofhe =
-                  j ? omv[j-1].degree + omv[j-1].firsthalfedge : 0;
-/* tu tez jest delikatny moment. Jesli stopien wierzcholka sie zmienil, to */
-/* znaczy, ze jakies jgo polkrawedzie wypadly. Wtedy ostatnia wychodzaca */
-/* polkrawedz ma byc bez pary, wiec przepisywac na ogol trzeba "od srodka", */
-/* nie od poczatku. Proponuje rozwiazanie, ktore w wersji dopuszczajacej */
-/* powielanie wierzcholkow trzeba bedzie zmodyfikowac. */
+    /*wierzcholki podwojone bede numerowac 'od tylu'*/
+  omvLastFreeSlot=*onv;
+  omvheiIter=0;
+  for ( i = 0; i < inv; i++ ) {
+    if ( vertexCopiesCounter[i] >= 1 ) {
+      isSecondOrNext = false;
+      f = imv[i].firsthalfedge;
       d = imv[i].degree;
-      if ( omv[j].degree < d ) {
-        m = d;
-        while ( isHalfedgeInResultingNet[imvhei[ifhe+m-1]] >= 0 )
-          m--;
-      }
-      else
-        m = 0;
-      for ( k = l = 0; k < d; k++ ) {
-        if ( m+k == d )
-          m -= d;
-        if ( isHalfedgeInResultingNet[imvhei[ifhe+m+k]] >= 0 ) {
-          omvhei[ofhe+l] = isHalfedgeInResultingNet[imvhei[ifhe+m+k]];
-          l ++;
+      for ( j = 0; j < d; j++ ) {
+        if ( isHalfedgeInResultingNet[imvhei[f+j]] >= 0 &&
+             ( imhe[imvhei[f+j]].otherhalf == -1 ||
+               isHalfedgeInResultingNet[imhe[imvhei[f+j]].otherhalf] < 0 ) ) {
+          if ( !isSecondOrNext ) {
+            isSecondOrNext = true;
+            currentVertex = isVertexInResultingNet[i];
+          }
+          else {
+            omvLastFreeSlot --;
+            currentVertex = omvLastFreeSlot;
+          }
+    /*iteruje po polkrawedziach taka jakby harmonijka, po drodze zmieniajac odpowiednie wartosci*/
+          omv[currentVertex].degree ++;
+          boundaryHalfedge = omvheiIter;
+          omvheiEntry = isHalfedgeInResultingNet[imvhei[f+j]];
+          heOut = imhe[imvhei[f+j]];
+          V0[imvhei[f+j]] = currentVertex;
+          face = heOut.facetnum;
+          for ( heIter = 0; heIter < imfac[face].degree; heIter++ ) {
+            if ( imhe[imfhei[imfac[face].firsthalfedge+heIter]].v1 == i ) {
+              heIn = imhe[imfhei[imfac[face].firsthalfedge+heIter]];
+              V1[imfhei[imfac[face].firsthalfedge+heIter]] = currentVertex;
+            }
+          }
+          heOut = imhe[heIn.otherhalf];
+          if ( heIn.otherhalf != -1 )
+            V0[heIn.otherhalf] = currentVertex;
+          face = heOut.facetnum;
+          while ( isFacetInResultingNet[face]>-1 && heIn.otherhalf >= 0 ) {
+            omv[currentVertex].degree ++;
+            omvhei[omvheiIter] = isHalfedgeInResultingNet[heIn.otherhalf];
+            omvheiIter ++;
+            for ( heIter = 0; heIter < imfac[face].degree; heIter++ ) {
+              if ( imhe[imfhei[imfac[face].firsthalfedge+heIter]].v1 == i ) {
+                heIn = imhe[imfhei[imfac[face].firsthalfedge+heIter]];
+                V1[imfhei[imfac[face].firsthalfedge+heIter]] = currentVertex;
+              }
+            }
+            heOut = imhe[heIn.otherhalf];
+            V0[heIn.otherhalf] = currentVertex;
+            face = heOut.facetnum;
+          }
+          omv[currentVertex].firsthalfedge = boundaryHalfedge;
+          omvhei[omvheiIter] = omvheiEntry;
+          zDrugiejStrony=2;
+          for ( a = omv[currentVertex].firsthalfedge;
+                a < omv[currentVertex].firsthalfedge+omv[currentVertex].degree/2;
+                a++ ) {
+            pom = omvhei[a];
+            omvhei[a] = omvhei[omv[currentVertex].firsthalfedge+omv[currentVertex].degree-zDrugiejStrony];
+            omvhei[omv[currentVertex].firsthalfedge+omv[currentVertex].degree-zDrugiejStrony] = pom;
+            zDrugiejStrony++;
+          }
+          omvheiIter++;
         }
       }
+    }
+    else if ( vertexCopiesCounter[i] == 0 ) {
+      f = imv[i].firsthalfedge;
+      d = imv[i].degree;
+      currentVertex = isVertexInResultingNet[i];
+      isSecondOrNext = false;
+      for ( j = 0; j < d; j++ ) {
+        if ( isHalfedgeInResultingNet[imvhei[f+j]] >= 0 ) {
+          if ( !isSecondOrNext ) {
+            omv[currentVertex].firsthalfedge = omvheiIter;
+            isSecondOrNext = true;
+          }
+          omv[currentVertex].degree ++;
+          heOut = imhe[imvhei[f+j]];
+          V0[imvhei[f+j]] = currentVertex;
+          omvhei[omvheiIter] = isHalfedgeInResultingNet[imvhei[f+j]];
+          omvheiIter ++;
+          face = heOut.facetnum;
+          for ( heIter = 0; heIter < imfac[face].degree; heIter++ ) {
+            if ( imhe[imfhei[imfac[face].firsthalfedge+heIter]].v1 == i ) {
+              heIn = imhe[imfhei[imfac[face].firsthalfedge+heIter]];
+              V1[imfhei[imfac[face].firsthalfedge+heIter]] = currentVertex;
+            }
+          }
+        }
+      }
+    }
+  }
+
+  for ( i = 0; i < inv; i++ ) {
+    if ( vertexCopiesCounter[i] == 0 )
+      vertexCopiesCounter[i]++;
+  }
+
+    /* wpisuje wierzcholki, w odpowiednim momencie je podwajajac */
+
+    /* przepisanie elementow nowej siatki */
+    /*optc*/
+  omvLastFreeSlot = *onv;
+  for ( i = 0; i < inv; i++ ) {
+    j = isVertexInResultingNet[i];
+    isSecondOrNext = false;
+    for ( k = 0; k < vertexCopiesCounter[i]; k++ ) {
+      if ( !isSecondOrNext ) {
+        memcpy ( optc+j*spdimen, iptc+i*spdimen, spdimen*sizeof(double) );
+        isSecondOrNext = true;
+      }
+      else {
+        omvLastFreeSlot--;
+        memcpy ( optc+omvLastFreeSlot*spdimen, iptc+i*spdimen, spdimen*sizeof(double) );
+      }
+    }
+  }
+
+    /*omhe*/
+  for ( i = 0; i < inhe; i++ ) {
+    j = isHalfedgeInResultingNet[i];
+    if ( j >= 0 ) {
+      omhe[j].facetnum = isFacetInResultingNet[imhe[i].facetnum];
+      k = imhe[i].otherhalf;
+      omhe[j].otherhalf = k >= 0 ? isHalfedgeInResultingNet[k] : -1;
+      omhe[j].v0 = V0[i];
+      omhe[j].v1 = V1[i];
     }
   }
 
@@ -287,27 +350,12 @@ skip:
       d = omfac[j].degree = imfac[i].degree;
       ifhe = imfac[i].firsthalfedge;
       omfac[j].firsthalfedge = ofhe =
-                  j ? omfac[j-1].degree + omfac[j-1].firsthalfedge : 0;
+           j ? omfac[j-1].degree + omfac[j-1].firsthalfedge : 0;
       for ( k = l = 0;  k < imfac[i].degree;  k++, l++ )
         omfhei[ofhe+l] = isHalfedgeInResultingNet[imfhei[ifhe+k]];
     }
   }
 
-    /* omhe */
-  for ( i = 0; i < inhe; i++ ) {
-    j = isHalfedgeInResultingNet[i];
-    if ( j >= 0 ) {
-      omhe[j].facetnum = isFacetInResultingNet[imhe[i].facetnum];
-      k = imhe[i].otherhalf;
-      omhe[j].otherhalf = k >= 0 ? isHalfedgeInResultingNet[k] : -1;
-      omhe[j].v0 = isVertexInResultingNet[imhe[i].v0];
-      omhe[j].v1 = isVertexInResultingNet[imhe[i].v1];
-    }
-  }
-
-  *onv = wielkoscomv;
-  *onhe = wielkoscomhe;
-  *onfac = wielkoscomfac;
   pkv_SetScratchMemTop ( sp );
   return true;
 
